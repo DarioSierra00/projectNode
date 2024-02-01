cf589203c90452f75011e0179e540600039cb055
DarioSierra00 <dario.sierramartinez@gmail.com>
2024-02-01 12:29:18 +0100

    subiendo

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -59,9 +59,9 @@ const editUser = async (req,res) =>{
 
 const deleteUser = async (req,res) =>{
     const id = req.params.id;
-    
     try {
-        const user = await User.findByIdAndDelete(id)
+        const user = await User.findByIdAndUpdate(id, {active: false});
+
         res.status(200).json(user)
     } catch (error) {
         res.status(500).json({message : error})
